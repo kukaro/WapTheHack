021606b8901c39941ba940b56d28e458fd78d26d
jinzzam <firstmind_@naver.com>
2018-08-09 03:39:27 +0900

    turn off led lights when enter 'o' in serial monitor

--- IoT-server/arduino/SerialTest/SerialTest.ino
+++ IoT-server/arduino/SerialTest/SerialTest.ino
@@ -3,7 +3,6 @@ const int outWaterPin = A1;
 const int gasPin = A2;
 const int lampLedPin = 6;
 const int fireLedPin = 7;
-const int testLedPin = 2;
 
 int inWater = 0;
 int outWater = 0;
@@ -13,29 +12,31 @@ bool fireLed = true;
 
 char income = 'x';
 
+bool sw = true; 
+
 void setup() {
   Serial.begin(9600);
   pinMode(lampLedPin, OUTPUT);
-  pinMode(fireLedPin, OUTPUT);    
-  pinMode(testLedPin, OUTPUT);   
+  pinMode(fireLedPin, OUTPUT);  
+  digitalWrite(lampLedPin, HIGH);
+  digitalWrite(fireLedPin, HIGH);  
 }
 
 void loop() {
   if(Serial.available()){
-    income = (char)Serial.read();
-  }
-  if(income=='o'){
+    income = Serial.read();
+    if(income=='o'){
     Serial.println(income);
     income = 'x';
     digitalWrite(lampLedPin, LOW);
-    
+    digitalWrite(fireLedPin, LOW);
+    }
   }
+  
   inWater = analogRead(inWaterPin);
   outWater = analogRead(outWaterPin);
   gas = analogRead(gasPin);
-  digitalWrite(lampLedPin, HIGH);
-  digitalWrite(fireLedPin, HIGH);
-
+  
   Serial.print("inWater : ");
   Serial.println(inWater);
   Serial.print("outWater : ");
@@ -47,7 +48,4 @@ void loop() {
   Serial.print("fire : ");
   Serial.println(fireLed);
   delay(1000);
-  digitalWrite(lampLedPin, HIGH);
-  digitalWrite(fireLedPin, HIGH);
-  digitalWrite(testLedPin, HIGH);
 }
