82e089d163275b26e93db63a7a63e2ef07f9622c
DESKTOP-V77LQF3\LeeJeongmin <ljm245@naver.com>
2018-08-08 18:53:23 +0900

    Render risk zone from db only data

--- webserver/routes/disasterAreaTest.js
+++ webserver/routes/disasterAreaTest.js
@@ -14,8 +14,14 @@ var areaModel = mongoose.model('disaster_area',riskZone);
 /* GET home page. */
 router.get('/', function(req, res, next) {
     areaModel.find({},function (err,data) {
-        //console.log(data);
-        res.render('disasterAreaTest', { title: 'Express',data:data });
+        var dataLength = data.length;
+        var areas = new Array();
+        var degree = new Array();
+        for(var i = 0;i<dataLength;i++){
+            areas[i]='주소 : '+ data[i].재해위험지역 + data[i].재해위험지역상세주소;
+            degree[i] = '위험 등급 : '+data[i].재해위험등급;
+        }
+        res.render('disasterAreaTest', { title: 'Express',data2:degree, data:areas });
     });
 });
 
