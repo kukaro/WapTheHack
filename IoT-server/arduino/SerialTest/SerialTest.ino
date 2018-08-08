33a67abf0b69cafb200dee24b196d3639587fa52
jinzzam <firstmind_@naver.com>
2018-08-09 03:02:11 +0900

    IoT test Scenario

--- IoT-server/arduino/SerialTest/SerialTest.ino
+++ IoT-server/arduino/SerialTest/SerialTest.ino
@@ -1,33 +1,53 @@
-const int inWaterPin = 0;
-const int outWaterPin = 2;
-const int gasPin = 4;
+const int inWaterPin = A0;
+const int outWaterPin = A1;
+const int gasPin = A2;
+const int lampLedPin = 6;
+const int fireLedPin = 7;
+const int testLedPin = 2;
+
 int inWater = 0;
 int outWater = 0;
 int gas = 0;
-String income = "";
+bool lampLed = true;
+bool fireLed = true;
+
+char income = 'x';
 
 void setup() {
   Serial.begin(9600);
-  pinMode(inWaterPin, OUTPUT);
-  pinMode(outWaterPin, OUTPUT);
+  pinMode(lampLedPin, OUTPUT);
+  pinMode(fireLedPin, OUTPUT);    
+  pinMode(testLedPin, OUTPUT);   
 }
 
 void loop() {
   if(Serial.available()){
-    income += (char)Serial.read();
+    income = (char)Serial.read();
   }
-  if(income!=0){
+  if(income=='o'){
     Serial.println(income);
-    income = "";
+    income = 'x';
+    digitalWrite(lampLedPin, LOW);
+    
   }
   inWater = analogRead(inWaterPin);
   outWater = analogRead(outWaterPin);
   gas = analogRead(gasPin);
+  digitalWrite(lampLedPin, HIGH);
+  digitalWrite(fireLedPin, HIGH);
+
   Serial.print("inWater : ");
   Serial.println(inWater);
   Serial.print("outWater : ");
   Serial.println(outWater);
   Serial.print("gas : ");
   Serial.println(gas);
+  Serial.print("lamp : ");
+  Serial.println(lampLed);
+  Serial.print("fire : ");
+  Serial.println(fireLed);
   delay(1000);
+  digitalWrite(lampLedPin, HIGH);
+  digitalWrite(fireLedPin, HIGH);
+  digitalWrite(testLedPin, HIGH);
 }
