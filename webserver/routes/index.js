7be58827562b111e29bfb3575d12b3fad0a6f15d
DESKTOP-V77LQF3\LeeJeongmin <ljm245@naver.com>
2018-08-10 16:43:13 +0900

    Send data ejs to js

--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.post('/test', (req,res,next)=>{
+  console.log(req.body);
+});
+
+router.post('/test2', (req,res,next)=>{
+    console.log(req.body);
+});
+
 module.exports = router;
