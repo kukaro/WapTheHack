ca6206790a154dd2d2e7d949043aee0b6d6bfb54
DESKTOP-V77LQF3\LeeJeongmin <ljm245@naver.com>
2018-08-11 20:36:38 +0900

    Return

--- webserver/public/js/demo/chart-area-demo.js
+++ webserver/public/js/demo/chart-area-demo.js
@@ -7,9 +7,9 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["3시간 전", "2시간 전", "1시간 전", "현재", "1시간 후", "2시간 후", "3시간 "],
+    labels: ["03시", "06시", "09시", "12시", "15시", "18시", "21시","24시"],
     datasets: [{
-      label: "Sessions",
+      label: "3시간강수량",
       lineTension: 0.3,
       backgroundColor: "rgba(2,117,216,0.2)",
       borderColor: "rgba(2,117,216,1)",
@@ -20,12 +20,31 @@ var myLineChart = new Chart(ctx, {
       pointHoverBackgroundColor: "rgba(2,117,216,1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [50, 60, 70, 65, 80, 87, 90, 93],
-    }],
+      data: []/*[50, 60, 70, 65, 80, 87, 90, 93]*/,
+    },
+        {
+            label: "강수확률",
+            lineTension: 0.3,
+            backgroundColor: 'rgba(255,0,0,0.1)',
+            borderColor: "rgba(255,0,0,1)",
+            pointRadius: 5,
+            pointBackgroundColor: "rgba(255,0,0,1)",
+            pointBorderColor: "rgba(255,255,255,0.8)",
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(255,0,0,1)",
+            pointHitRadius: 50,
+            pointBorderWidth: 2,
+            data: []/*[50, 60, 70, 65, 80, 87, 90, 93]*/,
+        }],
   },
   options: {
     scales: {
       xAxes: [{
+        scaleLabel:{
+          display:true,
+            labelString:'시간',
+            labelColor:'red'
+        },
         time: {
           unit: 'date'
         },
@@ -33,10 +52,14 @@ var myLineChart = new Chart(ctx, {
           display: false
         },
         ticks: {
-          maxTicksLimit: 7
+          maxTicksLimit: 8
         }
       }],
       yAxes: [{
+          scaleLabel:{
+              display:true,
+              labelString:'3시간 강수량(mm)/강수확률(%)'
+          },
         ticks: {
           min: 0,
           max: 100,
@@ -48,7 +71,12 @@ var myLineChart = new Chart(ctx, {
       }],
     },
     legend: {
-      display: false
+      display: true,
+        position:'top',
+        labels:{
+        boxWidth:80,
+            fontColor:'black'
+        }
     }
   }
 });
