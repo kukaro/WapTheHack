3907bbb95bbfe46f25778744923f875a50d29965
jinzzam <firstmind_@naver.com>
2018-08-11 22:28:15 +0900

    edit condition

--- webserver/app.js
+++ webserver/app.js
@@ -98,8 +98,7 @@ io.sockets.on('connection', function (socket) {
                 io.sockets.emit('sendMsg', {'msg': '2'});
             } else if ((700 < inWater && inWater < 900) || (700 < outWater && outWater < 900)) {
                 io.sockets.emit('sendMsg', {'msg': '3'});
-            } else (inWater < 900 || outWater < 900)
-            {
+            } else if (900 < inWater || 900 < outWater) {
                 io.sockets.emit('sendMsg', {'msg': '4'});
             }
         } catch (exception) {
